5231f3b3f4c5168b51982ee48600567baebc7b97
chaklader17 <aschaklader@gmail.com>
2025-08-11 13:11:34 +0600

    ui for degree progress

--- client/src/pages/DashboardPage.tsx
+++ client/src/pages/DashboardPage.tsx
@@ -4,8 +4,10 @@ import Navbar, { type ActiveNavItems } from '../components/dashboard/Navbar';
 import UserProfileCard from '../components/dashboard/UserProfileCard';
 import FacultyAdvisorCard from '../components/dashboard/FacultyAdvisorCard';
 import DashboardFooter from '../components/common/DashboardFooter';
+import DegreeProgressTracker from '../components/dashboard/DegreeProgressTracker';
 
 const DashboardPage: React.FC = () => {
+  // Static data for user profile
   const userData = {
     userName: "Samiyeel Alim Binaaf",
     userId: "2212779042",
@@ -15,11 +17,37 @@ const DashboardPage: React.FC = () => {
     avatarUrl: "https://rds3.northsouth.edu/assets/images/avatars/profile-pic.jpg",
   };
 
+  // Static data for faculty advisor
   const advisorData = {
     advisorId: "TNS1",
     room: "SAC8XX",
     name: "Tanzilah Noor Shabnam",
     email: "tanzilah.shabnam@northsouth.edu",
+  };
+
+  // Static data for the degree progress tracker
+  const degreeProgressData = {
+    totalCredits: 130,
+    completedCredits: 42,
+    completedCourses: [
+      { code: 'CSE115', title: 'Programming Language I', credits: 3 },
+      { code: 'CSE115L', title: 'Programming Language I Lab', credits: 1 },
+      { code: 'CSE173', title: 'Discrete Mathematics', credits: 3 },
+      { code: 'CSE215', title: 'Programming Language II', credits: 3 },
+      { code: 'CSE215L', title: 'Programming Language II Lab', credits: 1 },
+      { code: 'PHY107', title: 'Physics I', credits: 3 },
+      { code: 'ENG102', title: 'Introduction to Composition', credits: 3 },
+      { code: 'MAT120', title: 'Calculus I', credits: 3 },
+    ],
+    remainingCourses: [
+      { code: 'CSE225', title: 'Data Structures & Algorithms', credits: 3 },
+      { code: 'CSE231', title: 'Digital Logic Design', credits: 3 },
+      { code: 'CSE299', title: 'Junior Design Project', credits: 3 },
+      { code: 'CSE311', title: 'Database Systems', credits: 3 },
+      { code: 'CSE327', title: 'Software Engineering', credits: 3 },
+      { code: 'CSE332', title: 'Computer Organization', credits: 3 },
+      { code: 'CSE373', title: 'Design & Analysis of Algorithms', credits: 3 },
+    ],
   };
 
   const [currentNavItem, setCurrentNavItem] = useState<ActiveNavItems>("Home");
@@ -37,12 +65,15 @@ const DashboardPage: React.FC = () => {
         </div>
 
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
+          {/* Left Column: User Profile */}
           <div className="lg:col-span-1">
             <UserProfileCard {...userData} />
           </div>
+
+          {/* Right Column: Main Content */}
           <div className="lg:col-span-2 space-y-8">
             <FacultyAdvisorCard advisor={advisorData} />
-            {/* Future modules like Degree Progress Tracker and Grades Viewer will go here */}
+            <DegreeProgressTracker {...degreeProgressData} />
           </div>
         </div>
       </main>
